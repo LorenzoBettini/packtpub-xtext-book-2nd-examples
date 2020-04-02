a27c6527a290a9c4a9d854b8b61319f8032c295e
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2020-04-02 16:25:53 +0200

    39: Travis: build on Mac also when merging PR
    Task-Url: https://github.com/LorenzoBettini/packtpub-xtext-book-2nd-examples/issues/39

--- travis_build.sh
+++ travis_build.sh
@@ -7,17 +7,6 @@ set -ev
 # "The job exceeded the maximum log length, and has been terminated."
 MAVEN_ARGS="-B -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn"
 
-if [ "$TRAVIS_OS_NAME" == "osx" ]; then
-	if [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
-		echo "Build on MacOSX: Pull Request"
-		mvn $MAVEN_ARGS -f org.example.parent/pom.xml clean verify -Pall
-		# cd org.example.helloidea.parent && ./gradlew clean build
-	else
-		echo "Skipping build on MacOSX for standard commit"
-		#mvn -f org.example.parent/pom.xml clean verify
-	fi
-else
-	echo "Build on Linux"
-	mvn $MAVEN_ARGS -f org.example.parent/pom.xml clean verify -Pall
-	# cd org.example.helloidea.parent && ./gradlew clean build
-fi 
+mvn $MAVEN_ARGS -f org.example.parent/pom.xml clean verify -Pall
+# cd org.example.helloidea.parent && ./gradlew clean build
+
